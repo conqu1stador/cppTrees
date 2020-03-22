44900bff7fb8a28db7ffe0b4fbdb0d160788fe3e
Я <dmitriyKa3antsev@yandex.ru>
2020-03-22 15:39:34 +0300

    Update newRBST.cpp

--- newRBST.cpp
+++ newRBST.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <fstream>
+#include <vector>
 #include <ctime>
 #include <string>
 
@@ -7,18 +8,17 @@ template <typename T>
 struct TNode {
     TNode(T k) {
         data = k;
-        size = 1;
-        left = right = nullptr;
     }
-    int data, size;
-    TNode *left, *right;
+    T data;
+    int size = 1;
+    TNode *left = nullptr, *right = nullptr;
 };
-
-long long duplicate = 0;
 
 template <typename T>
 class RBST {
 public:
+    int insertCounter = 0;
+
     std::ofstream outTgf, help;
     RBST(T k) {
         root = new TNode<T>(k);
@@ -27,46 +27,46 @@ public:
         help << "#\n";
     }
 
-    TNode<T>* search(TNode<T>* node, T key) 
-    { 
-        if (node == nullptr || node->data == key) 
-            return root; 
-        
-        if (node->data < key) 
-            return search(node->right, key); 
+    TNode<T>* search(TNode<T>* node, T key)
+    {
+        if (node == nullptr || node->data == key)
+            return root;
 
-        return search(node->left, key); 
+        if (node->data < key)
+            return search(node->right, key);
+
+        return search(node->left, key);
     }
 
     int getSize(TNode<T>* node)
     {
-        if(node == nullptr) 
+        if(node == nullptr)
             return 0;
-        return node->size; 
+        return node->size;
     }
 
     void fixSize(TNode<T>* node)
     {
-        node->size = getSize(node->left) + getSize(node->right) + 1; 
+        node->size = getSize(node->left) + getSize(node->right) + 1;
     }
-   
+
     TNode<T>* rotateRight(TNode<T>* node)
     {
-        TNode<T>* temp = node->left; 
-        if(temp == nullptr) 
-            return node; 
-        node->left = temp->right; 
-        temp->right = node;  
-        temp->size = node->size; 
+        TNode<T>* temp = node->left;
+        if(temp == nullptr)
+            return node;
+        node->left = temp->right;
+        temp->right = node;
+        temp->size = node->size;
 	    fixSize(node);
-        root = temp; 
-        return temp; 
+        root = temp;
+        return temp;
     }
 
     TNode<T>* rotateLeft(TNode<T>* node)
     {
         TNode<T>* temp = node->right;
-        if(temp == nullptr) 
+        if(temp == nullptr)
             return node;
         node->right = temp->left;
         temp->left = node;
@@ -80,12 +80,12 @@ public:
     {
         if(node == nullptr) {
             TNode<T> *newNode = new TNode<T>(key);
-            return newNode; 
+            return newNode;
         }
-        
+
         if(node->data > key) {
-            node->left = insertRoot(node->left, key); 
-            return rotateRight(node); 
+            node->left = insertRoot(node->left, key);
+            return rotateRight(node);
         }
 
         else {
@@ -98,71 +98,89 @@ public:
     {
         if(node == nullptr) {
             TNode<T> *newNode = new TNode<T>(key);
-            return newNode; 
+            return newNode;
         }
 
-        if(rand() % (node->size + 1) == 0) 
-            return insertRoot(node, key);
+        // if(rand() % (node->size + 1) == 0) {
+        //     insertCounter++;   
+        //     return insertRoot(node, key);
+        // }
 
         if(node->left != nullptr && node->right != nullptr) {
-            if((node->left->size > node->right->size*2) || (node->left->size*2 < node->right->size))
-                return insertRoot(node, key);  
+            if(node->left->size < node->right->size*2)
+                if(rand() % (node->left->size + 1) == 0) {
+                    insertCounter++;
+                    return insertRoot(node, key);
+                }
+                    
+            if(node->left->size > node->right->size*2)
+                if(rand() % (node->right->size + 1) == 0) {
+                    insertCounter++;
+                    return insertRoot(node, key);
+                } 
         }
 
-        if(node->data > key) 
-            node->left = insert(node->left, key); 
+        // if(node->left != nullptr && node->right != nullptr) {
+        //    if((node->left->size > node->right->size*2) || (node->left->size*2 < node->right->size)) {
+        //        insertCounter++;
+        //        return insertRoot(node, key);
+        //    }
+        // }
+
+        if(node->data > key)
+            node->left = insert(node->left, key);
         else
-            node->right = insert(node->right, key); 
+            node->right = insert(node->right, key);
         fixSize(node);
-        root = node; 
-        return node; 
+        root = node;
+        return node;
     }
 
     TNode<T>* join(TNode<T>* node1, TNode<T>* node2)
     {
-        if(node1 == nullptr) 
+        if(node1 == nullptr)
             return node2;
-        if(node2 == nullptr) 
+        if(node2 == nullptr)
             return node1;
 
-        if(rand() % (node1->size + node2->size) < node1->size) 
+        if(rand() % (node1->size + node2->size) < node1->size)
         {
-            node1->right = join(node1->right, node2); 
+            node1->right = join(node1->right, node2);
             fixSize(node1);
             root = node1;
-            return node1; 
+            return node1;
         }
-        else 
+        else
         {
-            node2->left = join(node1,node2->left); 
+            node2->left = join(node1,node2->left);
             fixSize(node2);
             root = node2;
-            return node2; 
+            return node2;
         }
     }
 
     TNode<T>* remove(TNode<T>* node, T key)
     {
-        if(node == nullptr) 
-            return node; 
-        if(node->data == key) 
+        if(node == nullptr)
+            return node;
+        if(node->data == key)
         {
-            TNode<T>* temp = join(node->left, node->right); 
-            delete node; 
-            return temp; 
+            TNode<T>* temp = join(node->left, node->right);
+            delete node;
+            return temp;
         }
-        else if(key < node->data) 
-            node->left = remove(node->left, key); 
-        else 
+        else if(key < node->data)
+            node->left = remove(node->left, key);
+        else
             node->right = remove(node->right, key);
         root = node;
-        return node; 
+        return node;
     }
     int counter;
     void order(TNode<T>* node) //Прямой обход
     {
-        if (node != nullptr) 
-        { 
+        if (node != nullptr)
+        {
             //std::cout << node->data << '\n';
             outTgf << counter << " " << node->data << '\n';
             counter++;
@@ -175,9 +193,9 @@ public:
                 help << node->data << " " << node->right->data << '\n';
             }
             //std::cout << "-----" << '\n';
-            order(node->left);  
-            order(node->right); 
-        } 
+            order(node->left);
+            order(node->right);
+        }
     }
 
     TNode<T>* getRoot() {
@@ -185,12 +203,12 @@ public:
     }
 
     int height(TNode<T>* node) {
-        if (node == nullptr) 
+        if (node == nullptr)
             return 0;
         int left, right;
         if(node->left != nullptr)
             left = height(node->left);
-        else 
+        else
             left = -1;
         if(node->right != nullptr)
             right = height(node->right);
@@ -214,17 +232,16 @@ public:
     }
 
 private:
-    TNode<T>* root;
+    TNode<T>* root = new TNode<T>(0);
 };
 
 template <typename T>
 struct BTNode {
     BTNode(T k) {
         data = k;
-        left = right = nullptr;
     }
-    int data;
-    BTNode *left, *right;
+    T data;
+    BTNode *left = nullptr, *right = nullptr;
 };
 
 template <typename T>
@@ -237,92 +254,89 @@ public:
         help << "#\n";
     }
     std::ofstream outTgf, help;
-    BTNode<T>* search(BTNode<T>* node, T key) 
-    { 
-        if (node == nullptr || node->data == key) 
-            return root; 
-        
-        if (node->data < key) 
-            return search(node->right, key); 
+    BTNode<T>* search(BTNode<T>* node, T key)
+    {
+        if (node == nullptr || node->data == key)
+            return root;
 
-        return search(node->left, key); 
-    } 
+        if (node->data < key)
+            return search(node->right, key);
 
-    BTNode<T>* insert(BTNode<T>* node, T key) 
+        return search(node->left, key);
+    }
+
+    BTNode<T>* insert(BTNode<T>* node, T key)
     {
         if (node == nullptr) {
             BTNode<T> *newNode = new BTNode<T>(key);
             return newNode;
         }
-        
-        if (key == node->data)
-            duplicate++;
 
-        else if (key < node->data) 
+        else if (key < node->data)
             node->left = insert(node->left, key);
 
-        else if (key > node->data) 
+        else if (key > node->data)
             node->right = insert(node->right, key);
-            
-        return node; 
+
+        return node;
     }
 
-    BTNode<T>* minNodeData(BTNode<T>* node) 
-    { 
-        BTNode<T>* current = node; 
-        while (current->left != nullptr && current) 
-            current = current->left; 
-    
-        return current; 
-    } 
+    BTNode<T>* minNodeData(BTNode<T>* node)
+    {
+        BTNode<T>* current = node;
+        while (current->left != nullptr && current)
+            current = current->left;
 
-    BTNode<T>* deleteNode(BTNode<T>* node, T key) 
-    { 
-        if (node == nullptr) 
+        return current;
+    }
+
+    BTNode<T>* deleteNode(BTNode<T>* node, T key)
+    {
+        if (node == nullptr)
             return node;
 
-        if (key < node->data) 
-            node->left = deleteNode(node->left, key); 
+        if (key < node->data)
+            node->left = deleteNode(node->left, key);
 
-        else if (key > node->data) 
-            node->right = deleteNode(node->right, key); 
+        else if (key > node->data)
+            node->right = deleteNode(node->right, key);
 
         else
-        { 
-            if (node->left == nullptr) 
-            { 
-                BTNode<T>* temp = node->right; 
-                delete node; 
-                return temp; 
-            } 
-            else if (node->right == nullptr) 
-            { 
-                BTNode<T>* temp = node->left; 
+        {
+            if (node->left == nullptr)
+            {
+                BTNode<T>* temp = node->right;
                 delete node;
-                return temp; 
-            } 
+                return temp;
+            }
+            else if (node->right == nullptr)
+            {
+                BTNode<T>* temp = node->left;
+                delete node;
+                return temp;
+            }
 
-            BTNode<T>* temp = minNodeData(node->right); 
-            node->data = temp->data; 
-            node->right = deleteNode(node->right, temp->data); 
+            BTNode<T>* temp = minNodeData(node->right);
+            node->data = temp->data;
+            node->right = deleteNode(node->right, temp->data);
         }
 
-        return node; 
-    } 
-    
+        return node;
+    }
+
     void inorder(BTNode<T>* node) //Централизованный обход
-    { 
-        if (node != nullptr) 
-        { 
-            inorder(node->left); 
-            std::cout << node->data << '\n'; 
-            inorder(node->right); 
-        } 
+    {
+        if (node != nullptr)
+        {
+            inorder(node->left);
+            std::cout << node->data << '\n';
+            inorder(node->right);
+        }
     }
     void order(BTNode<T>* node) //Прямой обход
     {
-        if (node != nullptr) 
-        { 
+        if (node != nullptr)
+        {
             //std::cout << node->data << '\n';
             outTgf << counter << " " << node->data << '\n';
             counter++;
@@ -335,9 +349,9 @@ public:
                 help << node->data << " " << node->right->data << '\n';
             }
             //std::cout << "-----" << '\n';
-            order(node->left);  
-            order(node->right); 
-        } 
+            order(node->left);
+            order(node->right);
+        }
     }
 
     BTNode<T>* getRoot() {
@@ -358,12 +372,12 @@ public:
         }
     }
     int height(BTNode<T>* node) {
-        if (node == nullptr) 
+        if (node == nullptr)
             return 0;
         int left, right;
         if(node->left != nullptr)
             left = height(node->left);
-        else 
+        else
             left = -1;
         if(node->right != nullptr)
             right = height(node->right);
@@ -372,16 +386,16 @@ public:
         return (left > right ? left : right) + 1;
     }
 private:
-    BTNode<T>* root;
+    BTNode<T>* root = new BTNode<T>(0);
 };
 
 int main() {
     clock_t start = clock();
     srand(time(NULL));
-    const int size = 100;
-    int rbstArr[size], bstArr[size];
+    const int size = 1000;
+    int rbstArr[size], bstArr[size], insertCounter = 0;
     for(int k = 0; k != size; ++k) {
-        int rnd = rand() % (size*10); 
+        int rnd = rand() % (size*10);
         RBST<int> a(rnd);
         BST<int> c(rnd);
         int b[size];
@@ -389,13 +403,13 @@ int main() {
             int temp = rand() % (size*10);
             for(int j = 0; j != size; ++j) {
                 while(b[j] == temp) {
-                    temp = rand() % (size*10); 
+                    temp = rand() % (size*10);
                 }
             }
             b[i] = temp;
-            //std::cout << b[i] << ' '; 
+            //std::cout << b[i] << ' ';
         }
-        //std::cout << rnd << '\n'; 
+        //std::cout << rnd << '\n';
         for(int i = 0; i != size; ++i) {
             a.insert(a.getRoot(), b[i]);
         }
@@ -406,13 +420,14 @@ int main() {
         c.printFile();
         rbstArr[k] = a.height(a.getRoot());
         bstArr[k] = c.height(c.getRoot());
+        insertCounter += a.insertCounter;
     }
     std::cout << "RBST / BST \n";
     std::ofstream wordRbst, wordBst, wordDraw;
     wordRbst.open("wordRbst.txt"); wordBst.open("wordBst.txt");
     int countRbst = 0, countBst = 0, draw = 0;
     for(int i = 0; i != size; ++i) {
-        wordRbst << rbstArr[i] << "\n"; wordBst << bstArr[i] << '\n'; 
+        wordRbst << rbstArr[i] << "\n"; wordBst << bstArr[i] << '\n';
         if(rbstArr[i] < bstArr[i])  ++countRbst;
         if(rbstArr[i] > bstArr[i])  ++countBst;
         if(rbstArr[i] == bstArr[i])  ++draw;
@@ -420,7 +435,7 @@ int main() {
     std::cout << "RBST better " << countRbst << " / All " << size << '\n';
     std::cout << "BST better " << countBst << " / All " << size << '\n';
     std::cout << "draw " << draw << " / All " << size << '\n';
-    std::cout << "duplicate " << duplicate << '\n';
+    std::cout << "avg insertCounter " << insertCounter/size << '\n';
     clock_t end = clock();
     std::cout << (((double)end-start)/CLOCKS_PER_SEC)*1000 << "ms";
 }
